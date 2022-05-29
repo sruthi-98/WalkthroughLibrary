65120837aa4bb2f7d200f3c5db84bf35aa1a2e64
Sruthi Suresh <sruthisuresh321@gmail.com>
2022-05-29 19:26:54 +0530

    Update button click handlers, add event listeners for scroll and resize and handle position of popper

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ class Popper extends HTMLElement {
   #stepIndex = 0;
   #steps;
 
+  #popper;
   #title;
   #body;
   #back;
   #next;
+  #target;
 
   constructor(steps) {
     super();
@@ -15,18 +17,96 @@ class Popper extends HTMLElement {
     this.onClose = this.onClose.bind(this);
     this.onBack = this.onBack.bind(this);
     this.onNext = this.onNext.bind(this);
+    this.highlightTargetElement = this.highlightTargetElement.bind(this);
+    this.positionPopper = this.positionPopper.bind(this);
+    this.resizeHandler = this.resizeHandler.bind(this);
 
     this.setup();
     this.setContent();
+    this.highlightTargetElement();
+    this.positionPopper();
+  }
+
+  connectedCallback() {
+    window.addEventListener("scroll", this.positionPopper);
+    window.addEventListener("resize", this.resizeHandler);
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener("scroll", this.positionPopper);
+    window.removeEventListener("resize", this.resizeHandler);
   }
 
   set stepIndex(index) {
     this.#stepIndex = index;
   }
 
+  getCurrentElement() {
+    return this.#steps[this.#stepIndex].element;
+  }
+
+  resizeHandler() {
+    this.highlightTargetElement();
+    this.positionPopper();
+  }
+
+  highlightTargetElement() {
+    const currentElement = this.getCurrentElement();
+
+    if (!currentElement) return;
+
+    // Posiitoning the target element with a mirror element
+    currentElement.scrollIntoView({ behaviour: "smooth" });
+    const currentElementRect = currentElement.getBoundingClientRect();
+    const scrollTop = document.documentElement.scrollTop;
+    this.#target.style = `
+        top: ${scrollTop + currentElementRect.top}px;
+        right: ${currentElementRect.right}px;
+        bottom: ${currentElementRect.bottom}px;
+        left: ${currentElementRect.left}px;
+        width: ${currentElementRect.width}px;
+        height: ${currentElementRect.height}px;
+      `;
+    document.body.appendChild(this.#target);
+  }
+
+  positionPopper() {
+    const currentElement = this.getCurrentElement();
+
+    // Center fixed position when element is not defined
+    if (!currentElement) {
+      this.#popper.style = "";
+      this.#popper.classList.add("center");
+      return;
+    }
+
+    const currentElementRect = currentElement.getBoundingClientRect();
+    const popperRect = this.#popper.getBoundingClientRect();
+    const windowHeight = document.documentElement.clientHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const popperPlacement =
+      currentElementRect.bottom + popperRect.height > windowHeight
+        ? "top"
+        : "bottom";
+
+    this.#popper.style = `
+    top: ${
+      scrollTop +
+      (popperPlacement === "top"
+        ? currentElementRect.top - popperRect.height
+        : currentElementRect.bottom)
+    }px;
+      left: ${
+        currentElementRect.left +
+        (currentElementRect.width - popperRect.width) / 2
+      }px;
+    `;
+  }
+
   setup() {
     const popper = document.createElement("div");
     popper.classList.add("popper");
+    this.#popper = popper;
 
     const header = document.createElement("header");
     header.classList.add("popper__header");
@@ -68,6 +148,10 @@ class Popper extends HTMLElement {
     popper.appendChild(footer);
 
     this.appendChild(popper);
+
+    const target = document.createElement("div");
+    target.classList.add("popper__target");
+    this.#target = target;
   }
 
   setContent() {
@@ -80,23 +164,38 @@ class Popper extends HTMLElement {
   }
 
   onClose() {
+    this.#target.remove();
     this.remove();
   }
 
   onBack() {
-    if (this.#stepIndex === 0) this.onClose();
-    else {
-      this.#stepIndex = this.#stepIndex - 1;
-      this.setContent();
+    if (this.#stepIndex === 0) {
+      this.onClose();
+      return;
     }
+
+    const currentElement = this.getCurrentElement();
+    if (currentElement) this.#target.remove();
+    else this.#popper.classList.remove("center");
+    this.#stepIndex = this.#stepIndex - 1;
+    this.setContent();
+    this.highlightTargetElement();
+    this.positionPopper();
   }
 
   onNext() {
-    if (this.#stepIndex === this.#steps.length - 1) this.onClose();
-    else {
-      this.#stepIndex = this.#stepIndex + 1;
-      this.setContent();
+    if (this.#stepIndex === this.#steps.length - 1) {
+      this.onClose();
+      return;
     }
+
+    const currentElement = this.getCurrentElement();
+    if (currentElement) this.#target.remove();
+    else this.#popper.classList.remove("center");
+    this.#stepIndex = this.#stepIndex + 1;
+    this.setContent();
+    this.highlightTargetElement();
+    this.positionPopper();
   }
 }
 
